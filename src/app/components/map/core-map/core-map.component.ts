8896d1436cc1541c8f1178e6b4ada6c916ea387a
Yuan Fu <yuanf9@uci.edu>
2019-12-03 18:26:55 -0800

    other useful maps added

--- src/app/components/map/core-map/core-map.component.ts
+++ src/app/components/map/core-map/core-map.component.ts
@@ -36,10 +36,29 @@ export class CoreMapComponent implements OnInit {
   private layersControl = {
     baseLayers: this.layers,
     overlays: {
-       Highways: tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-                maxZoom: 18,
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                })
+      Highways: tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      Mountain_Elevation: tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        maxZoom: 16
+        }),
+      NASAGIBS_ModisTerraLSTDay: tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/MODIS_Terra_Land_Surface_Temp_Day/default/GoogleMapsCompatible_Level{maxZoom}/{z}/{y}/{x}.png', {
+        attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+        minZoom: 1,
+        maxZoom: 7,
+        opacity: 0.75
+        }),
+      // OpenWeatherMap_Wind: tileLayer('http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?appid=f155b82748c596a1a405d04c976c5604&\n' +
+      //   'fill_bound=true&opacity=0.6&palette=-65:821692;-55:821692;-45:821692;-40:821692;\n' +
+      //   '-30:8257db;-20:208cec;-10:20c4e8;0:23dddd;10:c2ff28;20:fff028;25:ffc228;30:fc8014', {
+      //   maxZoom: 19,
+      //   attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+      //   apiKey: 'f155b82748c596a1a405d04c976c5604',
+      //   opacity: 0.5
+      //   })
+
     }
   };
   private options: MapOptions = {
